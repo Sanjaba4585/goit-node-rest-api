ada5749a6d81d4464674968149fa4439372b02d4
Oleksandr Korniiychuk <adissk85@gmail.com>
2024-02-21 17:05:20 +0100

    deploy

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import mongoose from "mongoose";
 
 import contactsRouter from "./routes/contactsRouter.js";
 
@@ -21,6 +22,22 @@ app.use((err, req, res, next) => {
   res.status(status).json({ message });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+const DB_HOST =
+  "mongodb+srv://Sanjaba4585:S9Xvtz193mIYauKF@cluster0.caizffu.mongodb.net/books_reader?retryWrites=true&w=majority";
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running. Use our API on port: 3000");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server is running. Use our API on port: 3000");
+// });
